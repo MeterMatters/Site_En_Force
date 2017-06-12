6fadb1a51ecc08ba6749f0e3d8b133a53ef2b230
MeterMatters <Geoffroydubruel@hotmail.fr>
2017-06-12 11:29:50 +0200

    Ajout des onglets pour les capteurs et les pièces
    Différentes options sur chaque page qui permettent la gestion des capteurs et des pièces.

---
+++ Site/vue/objet.php
@@ -0,0 +1,31 @@
+
+  <div class="menu-vertical">
+      <div class="mv-item">
+        <a href="index.php?page=ajout_objet">
+
+        <p> Ajouter un objet </p>
+      </a>
+      </div>
+      <div class="mv-item">
+        <a href="index.php?page=modif_objet">    
+
+        <p> Modifier un objet </p>
+        </a>
+      </div>
+
+
+      <div class="mv-item">
+        <a href="index.php?page=supprime_objet"> 
+
+        <p> Supprimer un objet </p>
+      </a>
+      </div>
+      <div class="mv-item">
+        <a href="index.php?page=etat_objet">
+
+        <p> Tous les objets </p>
+        </a>
+      </div>
+  </div>
+
+
